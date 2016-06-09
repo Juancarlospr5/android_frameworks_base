c85d818b81462fa4baaf52eef4d79e32ce958b87
Juancarlospr5 <tumayordeseo5@gmail.com>
2016-06-09 01:10:15 -0400

    fix 3g/4g
    Let's not recreate the statusbar for our mod

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -489,6 +489,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 		mContext.getContentResolver(),
 		Settings.System.SHOW_FOURG,
 		0, UserHandle.USER_CURRENT) == 1;
+		mNetworkController.onConfigurationChanged();
 		DontStressOnRecreate();
 		} else if (uri.equals(Settings.System.getUriFor(
 		Settings.System.SHOW_THREEG))) {
@@ -496,6 +497,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 		mContext.getContentResolver(),
 		Settings.System.SHOW_THREEG,
 		0, UserHandle.USER_CURRENT) == 1;
+		mNetworkController.onConfigurationChanged();
 		DontStressOnRecreate();
             }
          update();
